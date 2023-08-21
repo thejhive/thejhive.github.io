bdf57c36aca4a855a39798628f70b6df439688c2
Jeremy Hsieh <jeremy.t.hsieh@gmail.com>
2023-08-20 22:19:01 -0700

    cleaned up the js

--- js/friday.js
+++ js/friday.js
@@ -1,10 +1,9 @@
-let todayInCalifornia = new Intl.DateTimeFormat('en-US', {weekday: 'long', timeZone : 'America/Los_Angeles'}).format(new Date());
 // because putting everything in one line is great for reading code later on wheeeeeeeee
 
-if (todayInCalifornia == "Friday") {
+if (new Intl.DateTimeFormat('en-US', {weekday: 'long', timeZone : 'America/Los_Angeles'}).format(new Date()) == "Friday") {
     document.getElementById('friday').innerHTML = "YES";
-    document.getElementById('friday-description').innerHTML = "Today is Friday in California. Shoot!"
+    document.getElementById('friday-description').innerHTML = "Today is Friday in California. Shoot!";
 } else {
     document.getElementById('friday').innerHTML = "NO";
-    document.getElementById('friday-description').innerHTML = "Today is not Friday in California."
+    document.getElementById('friday-description').innerHTML = "Today is not Friday in California.";
 }
